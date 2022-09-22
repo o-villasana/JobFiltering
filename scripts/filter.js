e54354e4f6412f02dd34b0d93120c8c8936dd539
Oscar Villasana <ovillasana@microsoft.com>
2022-09-21 18:07:31 -0700

    alt solution

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,5 +1,32 @@
 console.log("hello world");
 
+function regex(desc){
+    const reg_ex = /([0-3]){1}.*-?(3)?\+?\s(years?).*(experience).*/;
+    var res = reg_ex.test(desc);
+
+    console.log(res);
+
+    if(res == false){
+        alert('This Job does not match Years of Experience')
+    }
+}
+
+
+let lastUrl = location.href; 
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    onUrlChange();
+  }
+}).observe(document, {subtree: true, childList: true});
+ 
+function onUrlChange() {
+  var jobs_desc = document.getElementById("job-details").innerText;
+  regex(jobs_desc);
+}
+
+
 // Add YOE filter option
 function addFilter(){
     //Get filter buttons
@@ -11,13 +38,22 @@ function addFilter(){
     experience_filter.innerHTML = experience_filter.innerHTML.replace('Experience Level', 'Years of Experience')
 
     console.log(experience_filter)
-    #TODO
+    
+    //TODO
 }
 
 // Compare regex to job desc
-function regexValidate(desc){
+function validateJobDesc(num, i){
+    
+    var url = 'https://www.linkedin.com/jobs/view/' + num;
+    
+    let new_window;
+    new_window = window.open(url, '_blank', 'width=400,height=400 top=200,left=600');
 
-    return false;
+    var jobs_desc = new_window.document.getElementById("job-details");
+    console.log(jobs_desc.innerText)
+    new_window.close();
+    return true;
 }
 
 function filterJobs(){
@@ -30,11 +66,12 @@ function filterJobs(){
     // For each job, compare to regex
     for (let i = 0; i < jobs_list.childElementCount; i++){
         var job_num = jobs_list.children[i].getAttribute("data-occludable-job-id");
-        if (!regexValidate(job_num)) {
+        if (!regexValidate(job_num, i)) {
             jobs_to_delete.push(job_num)
         }
     }
 
+    // Delete jobs the user did not qualify for
     for (let i = 0; i < jobs_to_delete.length; i++)
     {
         var id = '[data-occludable-job-id="' + jobs_to_delete[i] + '"]';
